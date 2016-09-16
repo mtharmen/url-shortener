b9d1de89886bfb6240558ed257d9692511daf80e
Muhinthan <m.tharmendran@gmail.com>
2016-09-16 18:54:06 +0000

    Messy

--- server.js
+++ server.js
@@ -1,74 +1,53 @@
 var express = require('express');
+//var validUrl = require('valid-url');
 var mongo = require('mongodb').MongoClient;
 
 var app = express();
-var url = 'mongodb://localhost:27017/url-shortener';
 
 function generateCode() {
     return Math.floor(Math.random() * 10000).toString();
 }
 
-function newURL(database, url) {
-    var newLink = { original: url, mini: generateCode() };
+mongo.connect('mongodb://localhost:27017/url-shortener', function(err, db) {
+    if (err) throw err;
+    else { console.log('Connected to url-shortener.'); }
     
-    database.insert(newLink, function(err, doc) {
+    var linksDB = db.collection('links');
+
+    app.use('/', express.static(__dirname + '/styles'));
+    app.use('/new', express.static(__dirname + '/styles'));
     
-         if (err) throw err;
+    app.get('/new/:url(*)', function(req, res){
         
-         console.log('Added new entry:\n' + JSON.stringify(newLink));
-         return newLink
-    });
-}
-
-
-app.use('/', express.static(__dirname + '/styles'));
-app.use('/new', express.static(__dirname + '/styles'));
-
-app.get('/new/:url(*)', function(req, res){
-    
-    var inputURL = req.params.url;
-    var miniObj = {};
-
-    mongo.connect(url, function(err, db) {
-        if (err) throw err;
-        else console.log('Connected to url-shortener.');
-          
-        var linksDB = db.collection('links');
+        var inputURL = req.params.url;
         
-        linksDB.remove({});
-        
-        // // Checking if it already exists
-        // linksDB.find({ original: inputURL }).limit(1).toArray(function(err, doc){
-        //     if (err) throw err;
+        console.log('Beginning search');
+        // Checking if entry already exists
+        linksDB.find({ original: inputURL }, { original: 1, mini: 1, _id: 0 }).limit(1).toArray(function(err, documents) {
             
-        //     miniObj = { original: inputURL, mini: doc[0].mini };
-        //     console.log(JSON.stringify(miniObj));
+            if (err) throw err;
             
-        //     // Found
-        //     if (miniObj) {
-        //         console.log('Found:');
-        //         console.log(miniObj);
-        //     }
-            
-        //     // Not Found, generating new entry
-        //     else {
+            // Found an Entry
+            if (documents.length) {
+                console.log('Found Entry: ' + JSON.stringify(documents[0]));
+                res.json(documents[0]);
+            }
+            // Entry not Found
+            else {
+                console.log('Entry not found, creating new entry');
                 
-        //         var newLink = { original: inputURL, mini: generateCode() };
-    
-        //         linksDB.insert(newLink, function(err, doc) {
+                var newLink = { original: inputURL, mini: generateCode() };
                 
-        //              if (err) throw err;
-                    
-        //              console.log('Added new entry:\n' + newLink);
-        //              miniObj = newLink;
-        //         });
-        //     }
-        // });
-            
-        db.close();
-        res.json(miniObj);
+                // linksDB.insert(newLink, function(err, doc) {
+                //     if (err) throw err;
+                //     console.log('Added new entry: ' + JSON.stringify(newLink));
+                //     res.json(newLink);
+                // });
+                res.json(newLink);
+            }
+        });
     });
-
+    db.close();
 });
 
 app.listen(8080, function() {
